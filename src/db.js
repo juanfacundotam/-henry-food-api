d526f07fe64533944d6ea42dd8bc5833f8fc23e5
juanfacundotam <facutam@gmail.com>
2023-04-28 10:08:22 -0300

    api deploy

--- src/db.js
+++ src/db.js
@@ -9,14 +9,14 @@ const {
 // const DietModel = require("./models/Diet");
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
-// const sequelize = new Sequelize(DB_DEPLOY, {
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 
 
 const basename = path.basename(__filename);
